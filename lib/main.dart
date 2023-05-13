0d696377f6b19119462f8dc9727109ef592a9815
Maryam Behzadi <M.Behzadi@HC.com>
2023-05-13 11:09:15 +0330

    creating a default tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'DailyPackages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,27 +11,50 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-      statusBarBrightness: Brightness.dark,
-    ));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
         appBar: AppBar(
-          title: const Text('انتخاب بسته اینترنت'),
-        ),
-        body: const Center(
-          child: Text(
-            'Hello World',
-            textDirection: TextDirection.ltr,
+          title: Text('انتخاب بسته اینترنتی'),
+          bottom:  const TabBar(
+            tabs: [
+              Tab(text: 'روزانه',),
+              Tab(text: 'هفتگی',),
+              Tab(text: 'ماهانه',),
+              Tab(text: 'انواع دیگر',),
+            ],
           ),
         ),
+        body: TabBarView(
+          children: [
+            DailyPackages(),
+            Center(child: Text('Weekly Packages'),),
+            Center(child: Text('Monthly Packages'),),
+            Center(child: Text('Other Packages'),),
+          ],
+        )
       ),
     );
   }
 }
+
