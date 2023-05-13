4c98becbe46974e023f1257484b12d834ed31678
Maryam Behzadi <M.Behzadi@HC.com>
2023-05-13 11:10:04 +0330

    creating a defalt page for daily packages

---
+++ lib/DailyPackages.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DailyPackages extends StatelessWidget {
+  const DailyPackages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(' Daily Packages'),
+    );
+  }
+}
